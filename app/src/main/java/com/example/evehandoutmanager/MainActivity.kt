77efecb846247bbb5982e3ff954ac257a951eab0
Kurt <kurtw.wimer@gmail.com>
2022-08-22 22:21:55 -0600

    Pass the callback
    Fragment manager created programatically instead of XML to allow for passing args to the initial fragment

--- app/src/main/java/com/example/evehandoutmanager/MainActivity.kt
+++ app/src/main/java/com/example/evehandoutmanager/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.evehandoutmanager
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +14,28 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        Log.i("MainActivity",intent.data.toString())
+        if (intent.data != null) {
+            //extracts callback arguments to pass to character fragment
+            val uri = Uri.parse(intent.data.toString())
+            val code = uri.getQueryParameter("code")
+            val state = uri.getQueryParameter("state")
+            val callBackArgs = Bundle().apply {
+                putString("code", code)
+                putString("state", state)
+            }
+            //creates new navhost to pass initial arguments
+            val navFragment = NavHostFragment.create(R.navigation.nav_graph, callBackArgs)
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.navFragment, navFragment)
+                .setPrimaryNavigationFragment(navFragment) // equivalent to app:defaultNavHost="true"
+                .commit()
+            Log.i("MainActivity", intent.data.toString())
+        }else {
+            val navFragment = NavHostFragment.create(R.navigation.nav_graph)
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.navFragment, navFragment)
+                .setPrimaryNavigationFragment(navFragment) // equivalent to app:defaultNavHost="true"
+                .commit()
+        }
     }
 }
