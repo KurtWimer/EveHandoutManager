247756a74abb08f7eef317c1420db824ece3eb0f
Kurt <kurtw.wimer@gmail.com>
2022-09-09 17:48:35 -0600

    handout onRemoveButtonClick created
    setup handoutAdapter in Fragment

--- app/src/main/java/com/example/evehandoutmanager/home/HomeFragment.kt
+++ app/src/main/java/com/example/evehandoutmanager/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.example.evehandoutmanager.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,19 +10,33 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.example.evehandoutmanager.R
 import com.example.evehandoutmanager.databinding.FragmentHomeBinding
+import okhttp3.internal.toImmutableList
 
 class HomeFragment : Fragment() {
     private var _binding : FragmentHomeBinding? = null
     private val binding get() = _binding!!
     private val homeViewModel : HomeViewModel by activityViewModels()
+    private var adapter = HandoutAdapter(HandoutRemoveListener { handout: Handout ->
+        homeViewModel.onRemoveButtonClick(handout)
+    })
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        //bind data to viewModel
         _binding  = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
         binding.viewModel = homeViewModel
+
+        //Set Up Live Data Observers
+        homeViewModel.handoutList.observe(viewLifecycleOwner) {
+            it?.let {
+                Log.d("HandoutList", it.toString())
+                adapter.submitList(it.toImmutableList())
+            }
+        }
+        binding.handoutList.adapter = adapter
 
         return binding.root
     }
--- app/src/main/java/com/example/evehandoutmanager/home/HomeViewModel.kt
+++ app/src/main/java/com/example/evehandoutmanager/home/HomeViewModel.kt
@@ -2,11 +2,16 @@ package com.example.evehandoutmanager.home
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import com.example.evehandoutmanager.database.getDatabase
 
 class HomeViewModel(app: Application) : AndroidViewModel(app) {
     private val database = getDatabase(app)
-    val _Handouts = database.handoutDao.getHandouts()
+    val _HandoutList = database.handoutDao.getHandouts()
+    val handoutList : LiveData<List<Handout>>
+        get() = _HandoutList
 
-
+    fun onRemoveButtonClick(handout: Handout) {
+        database.handoutDao.delete(handout)
+    }
 }
