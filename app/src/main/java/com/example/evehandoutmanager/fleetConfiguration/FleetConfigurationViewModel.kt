5d0c77e3645893f3a25d9b9332f05e24e19250d3
Kurt <kurtw.wimer@gmail.com>
2022-09-18 12:00:38 -0600

    Bugfix prevent duplicate ISK value's in config

--- app/src/main/java/com/example/evehandoutmanager/fleetConfiguration/FleetConfigurationViewModel.kt
+++ app/src/main/java/com/example/evehandoutmanager/fleetConfiguration/FleetConfigurationViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.evehandoutmanager.fleetConfiguration
 
 import android.app.Application
+import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.evehandoutmanager.database.getDatabase
@@ -8,7 +9,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-class FleetConfigurationViewModel(app: Application) : AndroidViewModel(app) {
+class FleetConfigurationViewModel(private val app: Application) : AndroidViewModel(app) {
     private val database = getDatabase(app)
     var newConfig = FleetConfigItem()
 
@@ -25,6 +26,14 @@ class FleetConfigurationViewModel(app: Application) : AndroidViewModel(app) {
     fun onAddNewClick(item: FleetConfigItem){
         viewModelScope.launch {
             withContext(Dispatchers.IO) {
+                val currentConfig = database.fleetDao.getConfig()
+                //Prevent duplicate primary keys
+                for (config in currentConfig){
+                    if (config.iskValue == item.iskValue){
+                        Toast.makeText(app, "Cannot add ship with duplicate ISK value", Toast.LENGTH_LONG).show()
+                        return@withContext
+                    }
+                }
                 database.fleetDao.insert(item)
                 newConfig = FleetConfigItem()
             }
