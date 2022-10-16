970b89e0569326d502bdc6c2f4a064f84eba0b35
Kurt <kurtw.wimer@gmail.com>
2022-10-15 18:44:40 -0600

    Layout Bugfix Home Recyclerview no longer overlaps button Sample Data for screenshots

---
+++ app/src/main/java/com/example/evehandoutmanager/home/SampleData.kt
@@ -0,0 +1,17 @@
+package com.example.evehandoutmanager.home
+
+var sampleHandouts: List<Handout> = listOf<Handout>(
+    Handout(1, "Wreckevack t1", "Chribba", 0),
+    Handout(2, "Wreckevack t2", "Mittani", 1),
+    Handout(3, "Wreckevack Logi", "Zunk Zinkle", 3),
+    Handout(4, "Wreckevack t2", "Grath Telkin", 4),
+    Handout(5, "DOC Stiletto Tackle", "Suitonia", 5),
+    Handout(6, "Zippo", "Matterall", 6),
+    Handout(7, "Wreckevack Logi", "Hateless Gaming", 7),
+    Handout(8, "Wreckevack t1", "Headhunter JAX", 8),
+    Handout(9, "Wreckevack Links", "Runk Rinkle", 9),
+    Handout(10, "Wreckevack t2", "SirMolle", 10),
+    Handout(11, "DOC Malediction Tackle", "Kurtisthe1st", 11),
+    Handout(12,  "Wreckevack t2", "Kurtisthe3rd", 12),
+    Handout(13, "Wreckevack Logi", "Kurtisthe2st", 13)
+)
