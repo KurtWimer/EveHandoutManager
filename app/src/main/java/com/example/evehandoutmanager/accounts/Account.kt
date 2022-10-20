9cf050778bea724976c924b19f8d9a669bd951ab
Kurt <kurtw.wimer@gmail.com>
2022-10-19 19:32:24 -0600

    Refactor moved updating Access token to Account class

--- app/src/main/java/com/example/evehandoutmanager/accounts/Account.kt
+++ app/src/main/java/com/example/evehandoutmanager/accounts/Account.kt
@@ -2,9 +2,12 @@ package com.example.evehandoutmanager.accounts
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.auth0.android.jwt.JWT
+import com.example.evehandoutmanager.database.LocalDatabase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import com.example.evehandoutmanager.network.Esi
+import com.example.evehandoutmanager.network.Sso
 import retrofit2.await
 
 @Entity
@@ -14,7 +17,25 @@ data class Account constructor(
     val iconURL: String,
     val characterID: Int,
     var AccessToken: String,
-    var RefreshToken: String)
+    var RefreshToken: String){
+
+    suspend fun refreshAccountToken(clientID: String, database: LocalDatabase){
+        if (isTokenExpired(AccessToken)) { //Refresh Access Token Before Proceeding
+            val self = this
+            withContext(Dispatchers.IO) {
+                val newToken =
+                    Sso.retrofitInterface.refreshAccessToken(RefreshToken, clientID).await()
+                AccessToken = newToken.accessToken
+                RefreshToken = newToken.refreshToken
+                database.accountDao.update(self)
+            }
+        }
+    }
+
+    private fun isTokenExpired(accessToken : String): Boolean {
+        return requireNotNull(JWT(accessToken).isExpired(0))
+    }
+}
 
 //Utility function allows all com.example.evehandoutmanager.network calls to be done before creating a Character object
 suspend fun fetchInformation(characterID: String): Pair<String, String> {
